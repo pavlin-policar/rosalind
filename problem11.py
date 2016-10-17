2dfd123a7ceb7ae6c690faebc1ea0e8552a0f9ef
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-10-17 11:47:01 +0200

    Solve problem 11

---
+++ problem11.py
@@ -0,0 +1,32 @@
+CODON_TABLE = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G',
+}
+
+
+def chunks(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield(lst[i:i + chunk_size])
+
+
+if __name__ == '__main__':
+    string = input()
+    for triplet in chunks(string, 3):
+        protein = CODON_TABLE[triplet]
+        if protein == 'Stop':
+            break
+        print(protein, end='')
