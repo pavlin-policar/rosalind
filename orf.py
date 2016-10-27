8a72784b2c4dc2151055e5ebebf5101d5ea87a4a
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-10-27 11:03:53 +0200

    Fix orf finder

--- orf.py
+++ orf.py
@@ -1,16 +1,22 @@
 import sys
-from itertools import chain
-from re import finditer
+from re import findall
 
 from Bio import SeqIO
+from Bio.Seq import translate
+
+
+def get_orfs(seq):
+    orfs = set()
+    for strand in (seq, seq.reverse_complement()):
+        for offset in range(3):
+            matches = findall(
+                r'(?=(ATG(?:(?:\w{3})*?)(?:TAG|TGA|TAA)))',
+                str(strand)[offset:])
+            for match in matches:
+                orfs.add(translate(match, to_stop=True))
+    return orfs
+
 
 if __name__ == '__main__':
     record, = list(SeqIO.parse(sys.stdin, 'fasta'))
-    dna = record.seq
-
-    orfs = [[strand[x.start():].translate(to_stop=True)
-             for x in finditer('ATG', str(strand))]
-            for strand in (dna, dna.reverse_complement())]
-    orfs = {str(orf) for orf in chain(*orfs)}
-
-    print(*orfs, sep='\n')
+    print(*get_orfs(record.seq), sep='\n')
--- orfr.py
+++ orfr.py
@@ -1,15 +1,8 @@
-from itertools import chain
-
-from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
-from re import finditer
+from Bio.Seq import Seq
+
+from orf import get_orfs
 
 if __name__ == '__main__':
     dna = Seq(input(), alphabet=IUPAC.ambiguous_dna)
-
-    orfs = [[strand[x.start():].translate(to_stop=True)
-             for x in finditer('ATG', str(strand))]
-            for strand in (dna, dna.reverse_complement())]
-    orfs = {str(orf) for orf in chain(*orfs)}
-
-    print(max(map(str, orfs), key=len))
+    print(max(map(str, get_orfs(dna)), key=len))
