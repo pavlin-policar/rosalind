9f8cc4ea35b21a7acdafd338019c2de7d2d9e3a0
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-10-28 18:56:22 +0200

    Fix prob solution

--- prob.py
+++ prob.py
@@ -4,11 +4,10 @@ from math import log10
 
 if __name__ == '__main__':
     sequence = input()
-    array = input()
+    probabilites = map(float, input().split())
 
-    prob = lambda l, p: p / 2 if l in ('A', 'T') else (1 - p) / 2
+    prob = lambda l, p: p / 2 if l in ('C', 'G') else (1 - p) / 2
 
-    probabilites = [float(x) for x in array.split()]
     logs = [log10(reduce(operator.mul, [prob(l, p) for l in sequence], 1))
             for p in probabilites]
 
