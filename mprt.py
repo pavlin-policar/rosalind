8f2bbd14b780fe65a1e990ef08bcf8ee556d2043
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-10-29 09:37:52 +0200

    Solve mprt

---
+++ mprt.py
@@ -0,0 +1,20 @@
+import re
+import sys
+
+import requests
+
+if __name__ == '__main__':
+    codes = [l.strip() for l in sys.stdin]
+
+    strings = []
+    for code in codes:
+        res = requests.get(
+            'http://www.uniprot.org/uniprot/%s.fasta' % code).content.decode(
+            'utf-8')
+        split = res.split('\n')
+        _, *fasta = split
+        fasta = ''.join(fasta)
+        matches = list(re.finditer(r'(?=N[^P][ST][^P])', fasta))
+        if any(matches):
+            print(code)
+            print(' '.join([str(x.start() + 1) for x in matches]))
