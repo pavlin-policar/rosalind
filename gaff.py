0d3f097d7fc71a1fe91a43804e6a343c42efcbfe
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-11-09 19:29:23 +0100

    Improve gaff code

--- gaff.py
+++ gaff.py
@@ -26,17 +26,17 @@ def global_alignment(seq1, seq2, open_penalty, extend_penalty, score):
     # Computation
     for (i, ci), (j, cj) in product(enumerate(seq1), enumerate(seq2)):
         M[i, j], MT[i, j] = max(
-            (M[i - 1, j - 1] + score(ci, cj), ('M', (i - 1, j - 1))),
-            (X[i - 1, j - 1] + score(ci, cj), ('X', (i - 1, j - 1))),
-            (Y[i - 1, j - 1] + score(ci, cj), ('Y', (i - 1, j - 1))),
+            (M[i - 1, j - 1] + score(ci, cj), (MT, (i - 1, j - 1))),
+            (X[i - 1, j - 1] + score(ci, cj), (XT, (i - 1, j - 1))),
+            (Y[i - 1, j - 1] + score(ci, cj), (YT, (i - 1, j - 1))),
             key=operator.itemgetter(0))
         X[i, j], XT[i, j] = max(
-            (X[i - 1, j] - extend_penalty, ('X', (i - 1, j))),
-            (M[i - 1, j] - open_penalty, ('M', (i - 1, j))),
+            (X[i - 1, j] - extend_penalty, (XT, (i - 1, j))),
+            (M[i - 1, j] - open_penalty, (MT, (i - 1, j))),
             key=operator.itemgetter(0))
         Y[i, j], YT[i, j] = max(
-            (Y[i, j - 1] - extend_penalty, ('Y', (i, j - 1))),
-            (M[i, j - 1] - open_penalty, ('M', (i, j - 1))),
+            (Y[i, j - 1] - extend_penalty, (YT, (i, j - 1))),
+            (M[i, j - 1] - open_penalty, (MT, (i, j - 1))),
             key=operator.itemgetter(0))
     return M, MT, X, XT, Y, YT
 
@@ -49,18 +49,9 @@ def traceback(seq1, seq2, mi, MT, XT, YT):
     subs1, subs2 = deque(), deque()
     x, y = len(seq1) - 1, len(seq2) - 1
     while x >= 0 and y >= 0:
-        # Determine the active matrix
-        if mi == 'M':
-            am = MT
-        elif mi == 'X':
-            am = XT
-        else:
-            am = YT
-        # Append appropriate characters to the string
-        subs1.appendleft(seq1[x] if mi in ('M', 'X') else '-')
-        subs2.appendleft(seq2[y] if mi in ('M', 'Y') else '-')
-        # Update the matrix index,
-        mi, (x, y) = am[x, y]
+        subs1.appendleft(seq1[x] if mi is MT or mi is XT else '-')
+        subs2.appendleft(seq2[y] if mi is MT or mi is YT else '-')
+        mi, (x, y) = mi[x, y]
     return ''.join(subs1), ''.join(subs2)
 
 
@@ -72,5 +63,5 @@ if __name__ == '__main__':
         s1, s2, score=score_function, open_penalty=11, extend_penalty=1)
 
     pos = (len(s1) - 1, len(s2) - 1)
-    best_score, starting_t = max((M[pos], 'M'), (X[pos], 'X'), (Y[pos], 'Y'))
+    best_score, starting_t = max((M[pos], MT), (X[pos], XT), (Y[pos], YT))
     print(best_score, *traceback(s1, s2, starting_t, MT, XT, YT), sep='\n')
