1ba23ed27cf78239ba8ce095f664662094f10f47
Pavlin Poličar <pavlin.g.p@gmail.com>
2016-11-10 14:11:08 +0100

    Improve glob code

--- glob.py
+++ glob.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from collections import defaultdict
 from itertools import product
 
@@ -6,22 +7,27 @@ from Bio import SeqIO
 from Bio.SubsMat.MatrixInfo import blosum62
 
 
-def global_alignment(seq1, seq2, penalty=1):
+def global_alignment(seq1, seq2, score):
     """Calculate global alignment with linear gap penalty using a given matrix.
     """
-    dpt = defaultdict(int)
-    dpt.update({(i, -1): ((i + 1) * -penalty) for i in range(len(seq1))})
-    dpt.update({(-1, i): ((i + 1) * -penalty) for i in range(len(seq2))})
-    for (i, ci), (j, cj) in product(enumerate(seq1), enumerate(seq2)):
-        dpt[i, j] = max(
-            dpt[i - 1, j] - penalty,
-            dpt[i, j - 1] - penalty,
-            dpt[i - 1, j - 1] + blosum62.get((ci, cj), blosum62.get((cj, ci))))
+    dpt = defaultdict(lambda: -math.inf)
+    for i, j in product(range(-1, len(seq1)), range(-1, len(seq2))):
+        dpt[i, j] = 0 if (i, j) == (-1, -1) else max(
+            dpt[i - 1, j] + score(seq1[i], '-'),
+            dpt[i, j - 1] + score('-', seq2[j]),
+            dpt[i - 1, j - 1] + score(seq1[i], seq2[j]))
     return dpt
+
+
+def score_function(x, y):
+    if x == '-' or y == '-':
+        return -5
+    else:
+        return blosum62.get((x, y), blosum62.get((y, x)))
 
 
 if __name__ == '__main__':
     records = list(SeqIO.parse(sys.stdin, format='fasta'))
     s1, s2 = records[0].seq, records[1].seq
-    M = global_alignment(s1, s2, penalty=5)
+    M = global_alignment(s1, s2, score_function)
     print(M[len(s1) - 1, len(s2) - 1])
