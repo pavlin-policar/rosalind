fd36fa49b1d9f871e34742de3fa93a9b1adf32ec
Pavlin Poličar <pavlin.g.p@gmail.com>
2020-11-22 16:51:14 +0100

    Solve dbru

--- dbru.py
+++ dbru.py
@@ -1,18 +1,20 @@
 import sys
+from collections import defaultdict
+
 from Bio.Seq import Seq
 
 
 def de_bruijn(seqs, reverse_complement=False):
-    paths = {}
+    paths = set()
     for seq in seqs:
-        paths[str(seq[:-1])] = str(seq[1:])
+        paths.add((str(seq[:-1]), str(seq[1:])))
         if reverse_complement:
             rev = seq.reverse_complement()
-            paths[str(rev[:-1])] = str(rev[1:])
-    return paths
+            paths.add((str(rev[:-1]), str(rev[1:])))
+    return list(paths)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     seqs = [Seq(line.strip()) for line in sys.stdin]
-    for x, y in sorted(de_bruijn(seqs, reverse_complement=True).items()):
-        print('(%s, %s)' % (x, y))
+    for x, y in sorted(de_bruijn(seqs, reverse_complement=True)):
+        print("(%s, %s)" % (x, y))
