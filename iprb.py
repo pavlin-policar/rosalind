7026cedcb3a5fb32a874b2147e7effddd988b2f2
Pavlin Poličar <pavlin.g.p@gmail.com>
2021-10-30 18:30:44 +0200

    Update iprb

--- iprb.py
+++ iprb.py
@@ -3,15 +3,20 @@ def mendel(dominant, mixed, recessive):
     population = dominant + mixed + recessive
 
     p = (
-        dominant / population,
+        dominant / population * (dominant - 1) / (population - 1) * 1,
+        dominant / population * mixed / (population - 1) * 1,
+        dominant / population * recessive / (population - 1) * 1,
+
         mixed / population * dominant / (population - 1),
         mixed / population * (mixed - 1) / (population - 1) * .75,
         mixed / population * recessive / (population - 1) * .5,
+
         recessive / population * dominant / (population - 1),
         recessive / population * mixed / (population - 1) * .5,
+        recessive / population * recessive / (population - 1) * 0,
     )
     return sum(p)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(mendel(*map(int, input().split())))
